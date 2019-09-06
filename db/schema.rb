e01433e723854ca34a89e89cd7f6083aa24d23ec
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-09-06 03:18:48 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190906014227) do
+ActiveRecord::Schema.define(version: 2019_09_06_014227) do
 
 end
